87ec511cc6dfa40843eb0ae131847ad9f483bf93
GazarianGorik <gazariangorik@gmail.com>
2025-08-31 01:52:13 +0200

    license updated

--- AppSettings.cs
+++ AppSettings.cs
@@ -1,20 +1,16 @@
-﻿/****************************************************************************** 
- Copyright (c) 2025 Gorik Gazarian
- 
- This file is part of WwiseHDRTool.
- 
- Licensed under the PolyForm Noncommercial License 1.0.0.
-
- You may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- https://polyformproject.org/licenses/noncommercial/1.0.0
- and in the LICENSE file in this repository.
- 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on
- an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- either express or implied. See the License for the specific
- language governing permissions and limitations under the License.
+﻿/******************************************************************************
+#                                                                             #
+#   Copyright (c) 2025 Gorik Gazarian                                         #
+#                                                                             #
+#   This software is licensed under the PolyForm Internal Use License 1.0.0.  #
+#   You may obtain a copy of the License at                                   #
+#   https://polyformproject.org/licenses/internal-use/1.0.0                   #
+#   and in the LICENSE file in this repository.                               #
+#                                                                             #
+#   You may use, copy, and modify this software for internal purposes,        #
+#   including internal commercial use, but you may not redistribute it        #
+#   or sell it without a separate license.                                    #
+#                                                                             #
 ******************************************************************************/
 
 using LiveChartsCore.SkiaSharpView.Painting;

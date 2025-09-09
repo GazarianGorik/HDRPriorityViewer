a73869d5885cf0ca0b00b84f1f5e41cf70ab63b1
GazarianGorik <gazariangorik@gmail.com>
2025-09-09 11:06:11 +0200

    Better crash handling + Newtonsoft.Json updated to version 13.0.4-beta1

--- HDRPriorityViewer/App.xaml.cs
+++ HDRPriorityViewer/App.xaml.cs
@@ -13,7 +13,10 @@
 #                                                                             #
 ******************************************************************************/
 
+using System;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,12 +30,27 @@ namespace HDRPriorityViewer
     {
         public Window MainWindow => m_window!;
 
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
+
+            // UI thread exceptions
+            this.UnhandledException += App_UnhandledException;
+
+            // Task exceptions
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            // Non-UI thread exceptions
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+
             this.InitializeComponent();
         }
 
@@ -42,10 +60,38 @@ namespace HDRPriorityViewer
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            try
+            {
+                m_window = new MainWindow();
+                m_window.Activate();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox(IntPtr.Zero, ex.ToString(), "Crash au démarrage", 0);
+            }
         }
 
         private Window? m_window;
+
+
+        private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            Log.Error(e.Exception);
+            e.Handled = true; // empêche le crash brutal
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Log.Error(e.Exception);
+            e.SetObserved();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
     }
 }
